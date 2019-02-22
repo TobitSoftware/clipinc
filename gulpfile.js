aa2770b9e511d6b8527bf1a37c339c3af9341db7
Lucas Engelke <L-Engelke@web.de>
2019-02-22 11:38:29 +0100

    Fix build config

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,21 @@ const uglify = require('gulp-uglify');
 const pump = require('pump');
 const babel = require('gulp-babel');
 
-gulp.task('default', ['build', 'copy']);
+gulp.task('default', ['build', 'copy', 'copyEncoders']);
 
 gulp.task('copy', function (cb) {
     pump([
             gulp.src(['src/**/*', '!src/**/*.js']),
             gulp.dest('build')
+        ],
+        cb
+    );
+});
+
+gulp.task('copyEncoders', function (cb) {
+    pump([
+            gulp.src(['src/encoders/*']),
+            gulp.dest('build/encoders')
         ],
         cb
     );
